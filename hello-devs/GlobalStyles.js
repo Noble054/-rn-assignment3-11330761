b2e8e0a1cdc81469437cdd845925ba148c437343
Noble054 <atetteh325@st.ug.edu.gh>
2024-06-02 22:25:20 +0000

    Create GlobalStyles.js

---
+++ hello-devs/GlobalStyles.js
@@ -0,0 +1,23 @@
+/* fonts */
+export const FontFamily = {
+  latoRegular: "Lato-Regular",
+  latoBold: "Lato-Bold",
+  latoLight: "Lato-Light",
+};
+/* font sizes */
+export const FontSize = {
+  size_xs: 12,
+  size_base: 16,
+  size_xl: 20,
+};
+/* Colors */
+export const Color = {
+  colorBlack: "#000",
+  colorSnow: "#fbf9f7",
+  colorBisque: "#e8d1ba",
+};
+/* border radiuses */
+export const Border = {
+  br_mini: 15,
+  br_sm: 14,
+};
